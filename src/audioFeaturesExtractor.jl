f55ff4f08b13c79ca44681cc185b6c47927f53cb
giopaglia <24519853+giopaglia@users.noreply.github.com>
2024-03-01 12:33:33 +0100

    Add TODO, ci, julia formatter, deps

--- src/audioFeaturesExtractor.jl
+++ src/audioFeaturesExtractor.jl
@@ -1,148 +1,149 @@
 function audio_features_extractor(
-    x::AbstractVector{T};
-    sr::Int64,
-    dataset::Symbol=:all,
+	x::AbstractVector{T};
+	sr::Int64,
+	profile::Symbol = :all,
 
-    # fft
-    fft_length::Int64=256,
-    window_type::Vector{Symbol}=[:hann, :periodic],
-    window_length::Int64=fft_length,
-    overlap_length::Int64=Int(round(fft_length * 0.500)),
-    # window_length::Int64 = Int(round(0.03 * sr)),
-    # overlap_length::Int64 = Int(round(0.02 * sr)),
-    window_norm::Bool=:false,
+	# fft
+	fft_length::Int64 = 256,
+	window_type::Vector{Symbol} = [:hann, :periodic],
+	window_length::Int64 = fft_length,
+	overlap_length::Int64 = Int(round(fft_length * 0.500)),
+	# window_length::Int64 = Int(round(0.03 * sr)),
+	# overlap_length::Int64 = Int(round(0.02 * sr)),
+	window_norm::Bool = :false,
 
-    # spectrum
-    frequency_range::Vector{Int64}=Int[0, sr/2],
-    spectrum_type::Symbol=:power,
+	# spectrum
+	frequency_range::Vector{Int64} = Int[0, sr/2],
+	spectrum_type::Symbol = :power,
 
-    # mel
-    mel_style::Symbol=:htk,
-    mel_bands::Int64=26,
-    filterbank_design_domain::Symbol=:linear,
-    filterbank_normalization::Symbol=:bandwidth,
-    frequency_scale::Symbol=:mel,
+	# mel
+	mel_style::Symbol = :htk,
+	mel_bands::Int64 = 26,
+	filterbank_design_domain::Symbol = :linear,
+	filterbank_normalization::Symbol = :bandwidth,
+	frequency_scale::Symbol = :mel,
 
-    # mfcc
-    num_coeffs::Int64=13,
-    normalization_type::Symbol=:dithered,
-    rectification::Symbol=:log,
-    log_energy_source::Symbol=:standard,
-    log_energy_pos::Symbol=:replace,
-    delta_window_length::Int64=9,
-    delta_matrix::Symbol=:transposed,
+	# mfcc
+	num_coeffs::Int64 = 13,
+	normalization_type::Symbol = :dithered,
+	rectification::Symbol = :log,
+	log_energy_source::Symbol = :standard,
+	log_energy_pos::Symbol = :replace,
+	delta_window_length::Int64 = 9,
+	delta_matrix::Symbol = :transposed,
 
-    # spectral
-    spectral_spectrum::Symbol=:linear
-) where {T<:AbstractFloat}
+	# spectral
+	spectral_spectrum::Symbol = :linear,
+) where {T <: AbstractFloat}
 
-    setup = signal_setup(
-        sr=sr,
+	setup = signal_setup(
+		sr = sr,
 
-        # fft
-        window_type=window_type,
-        window_length=window_length,
-        overlap_length=overlap_length,
-        window_norm=window_norm,
+		# fft
+		window_type = window_type,
+		window_length = window_length,
+		overlap_length = overlap_length,
+		window_norm = window_norm,
 
-        # spectrum
-        frequency_range=frequency_range,
-        spectrum_type=spectrum_type,
+		# spectrum
+		frequency_range = frequency_range,
+		spectrum_type = spectrum_type,
 
-        # mel
-        mel_style=mel_style,
-        mel_bands=mel_bands,
-        filterbank_design_domain=filterbank_design_domain,
-        filterbank_normalization=filterbank_normalization,
-        frequency_scale=frequency_scale,
+		# mel
+		mel_style = mel_style,
+		mel_bands = mel_bands,
+		filterbank_design_domain = filterbank_design_domain,
+		filterbank_normalization = filterbank_normalization,
+		frequency_scale = frequency_scale,
 
-        # mfcc
-        num_coeffs=num_coeffs,
-        normalization_type=normalization_type,
-        rectification=rectification,
-        log_energy_source=log_energy_source,
-        log_energy_pos=log_energy_pos,
-        delta_window_length=delta_window_length,
-        delta_matrix=delta_matrix,
+		# mfcc
+		num_coeffs = num_coeffs,
+		normalization_type = normalization_type,
+		rectification = rectification,
+		log_energy_source = log_energy_source,
+		log_energy_pos = log_energy_pos,
+		delta_window_length = delta_window_length,
+		delta_matrix = delta_matrix,
 
-        # spectral
-        spectral_spectrum=spectral_spectrum
-    )
+		# spectral
+		spectral_spectrum = spectral_spectrum,
+	)
 
-    # convert to Float64
-    x = Float64.(x)
+	# convert to Float64
+	x = Float64.(x)
 
-    # preemphasis
-    # not siutable for our kind of experiments, maybe for speaker recognition: needs to look over it.
-    # zi = 2 * x[1] - x[2]
-    # filt!(x, [1.0, -0.97], 1.0, x, [zi])
-    # normalize
-    # x = x ./ maximum(abs.(x))
+	# preemphasis
+	# not siutable for our kind of experiments, maybe for speaker recognition: needs to look over it.
+	# zi = 2 * x[1] - x[2]
+	# filt!(x, [1.0, -0.97], 1.0, x, [zi])
+	# normalize
+	# x = x ./ maximum(abs.(x))
 
-    data = signal_data(
-        x=x
-    )
+	data = signal_data(
+		x = x,
+	)
 
-    takeFFT(data, setup)
-    lin_spectrogram(data, setup)
-    mel_spectrogram(data, setup)
-    _mfcc(data, setup)
-    spectral_features(data, setup)
-    f0(data, setup) # pay attention to fft length!
+	takeFFT(data, setup)
+	lin_spectrogram(data, setup)
+	mel_spectrogram(data, setup)
+	_mfcc(data, setup)
+	spectral_features(data, setup)
+	f0(data, setup) # pay attention to fft length!
 
-    # TODO verificare che il sample sia di lunghezza superiore a fft_length
+	# TODO verificare che il sample sia di lunghezza superiore a fft_length
 
-    if dataset == :full
-        vcat((
-            data.mel_spectrogram',
-            data.mfcc_coeffs',
-            data.mfcc_delta',
-            data.mfcc_deltadelta',
-            data.spectral_centroid',
-            data.spectral_crest',
-            data.spectral_decrease',
-            data.spectral_entropy',
-            data.spectral_flatness',
-            data.spectral_flux',
-            data.spectral_kurtosis',
-            data.spectral_rolloff',
-            data.spectral_skewness',
-            data.spectral_slope',
-            data.spectral_spread', 
-            data.f0'
-        )...)
+	if profile == :full
+		vcat(
+			(
+				data.mel_spectrogram',
+				data.mfcc_coeffs',
+				data.mfcc_delta',
+				data.mfcc_deltadelta',
+				data.spectral_centroid',
+				data.spectral_crest',
+				data.spectral_decrease',
+				data.spectral_entropy',
+				data.spectral_flatness',
+				data.spectral_flux',
+				data.spectral_kurtosis',
+				data.spectral_rolloff',
+				data.spectral_skewness',
+				data.spectral_slope',
+				data.spectral_spread',
+				data.f0',
+			)...,
+		)
 
-    elseif dataset == :gender
-        vcat((
-            data.mfcc_coeffs',
-            data.mfcc_delta',
-            data.mel_spectrogram[:, 1:13]'
-        )...)
+	elseif profile == :gender_recognition
+		vcat((
+			data.mfcc_coeffs',
+			data.mfcc_delta',
+			data.mel_spectrogram[:, 1:13]', # TODO: 13 -> a function of mel_bands or num_coeffs
+		)...)
 
-    elseif dataset == :speaker_recognition
-        vcat((
-            # data.mel_spectrogram[:, 1:26]',
-            data.mfcc_coeffs',
-            data.mfcc_delta',
-            data.mfcc_deltadelta',
-            # data.spectral_centroid', #NO
-            data.spectral_crest', #FORSE
-            data.spectral_decrease', #SI
-            # data.spectral_entropy', #NO
-            # data.spectral_flatness', #NO
-            data.spectral_flux', #FORSE
-            data.spectral_kurtosis', #FORSE
-            # data.spectral_rolloff', #NO
-            data.spectral_skewness', #FORSE
-            # data.spectral_slope', #NO
-            # data.spectral_spread', #NO
-            data.f0' #SI
-        )...)
+	elseif profile == :speaker_recognition
+		vcat((
+			# data.mel_spectrogram[:, 1:26]',
+			data.mfcc_coeffs',
+			data.mfcc_delta',
+			data.mfcc_deltadelta',
+			# data.spectral_centroid', #NO
+			data.spectral_crest', #FORSE
+			data.spectral_decrease', #SI
+			# data.spectral_entropy', #NO
+			# data.spectral_flatness', #NO
+			data.spectral_flux', #FORSE
+			data.spectral_kurtosis', #FORSE
+			# data.spectral_rolloff', #NO
+			data.spectral_skewness', #FORSE
+			# data.spectral_slope', #NO
+			# data.spectral_spread', #NO
+			data.f0', #SI
+		)...)
 
-    else
-        error("Unknown dataset type: $dataset.")
-    end
-
+	else
+		error("Unknown feature extraction profile: $profile.")
+	end
 end
 
 # bitmask approach
