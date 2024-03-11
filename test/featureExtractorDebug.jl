d51da704bf153b8b414af39f6fb3b67d14ae7ac8
PasoStudio73 <paso.studio73@gmail.com>
2024-03-11 22:45:48 +0100

    rage against af

--- test/featureExtractorDebug.jl
+++ test/featureExtractorDebug.jl
@@ -2,59 +2,57 @@ using DSP
 using FFTW
 using Parameters
 using PyCall
+using LinearAlgebra
 
-using SoleAudio
+# using SoleAudio
 
-# include("../src/windowing/windowing.jl")
-# include("../src/windowing/windows.jl")
-# include("../src/signalDataStructure.jl")
-# include("../src/fft/fft.jl")
-# include("../src/fft/lin.jl")
-# include("../src/fft/mel.jl")
-# include("../src/fft/spectral.jl")
-# include("../src/fft/f0.jl")
+include("../src/windowing/windowing.jl")
+include("../src/windowing/windows.jl")
+include("../src/signalDataStructure.jl")
+include("../src/fft/fft.jl")
+include("../src/fft/lin.jl")
+include("../src/fft/mel.jl")
+include("../src/fft/spectral.jl")
+include("../src/fft/f0.jl")
+include("../src/utils/in_out.jl")
+
 
 # af = pyimport_conda("audioflux")
-librosa = pyimport_conda("librosa")
+librosa = pyimport("librosa")
 
-sr_src = 16000
-x, sr = load_audio("/home/riccardopasini/.julia/dev/SoleAudio.jl/test/common_voice_en_23616312.wav", sr=sr_src)
-FFTLength = 256
-mel_num = 26
+sr_src = 8000
+x, sr = librosa.load("/home/riccardopasini/.julia/dev/SoleAudio.jl/test/common_voice_en_23616312.wav", sr=sr_src, mono=true)
+fft_length = 256
+frequency_range=Int[0, sr/2]
+mel_bands = 26
+num_coeffs = 13
 
 setup = signal_setup(
     sr=sr,
-
     # fft
     window_type=[:hann, :periodic],
-    window_length=FFTLength,
-    overlap_length=Int(round(FFTLength * 0.500)),
-    # window_length=Int(round(0.03 * sr)),
-    # overlap_length=Int(round(0.02 * sr)),
-    window_norm=:false,
-
+    window_length=fft_length,
+    overlap_length=Int(round(fft_length * 0.500)),
+    window_norm=false,
     # spectrum
-    frequency_range=Int[0, sr/2],
-    spectrum_type=:power,
-
+    frequency_range=frequency_range,
+    spectrum_type=:power, # :power, :magnitude
     # mel
-    mel_style=:htk,
-    mel_bands=mel_num,
+    mel_style=:htk, # :htk, :slaney
+    mel_bands=mel_bands,
     filterbank_design_domain=:linear,
-    filterbank_normalization=:bandwidth,
+    filterbank_normalization=:bandwidth, # :bandwidth, :area, :none
     frequency_scale=:mel,
-
     # mfcc
-    num_coeffs=13,
-    normalization_type=:dithered,
+    num_coeffs=num_coeffs,
+    normalization_type=:dithered, # :standard, :dithered
     rectification=:log,
-    log_energy_source=:standard,
-    log_energy_pos=:replace,
+    log_energy_source=:standard, # :standard (after windowing), :mfcc
+    log_energy_pos=:none, #:append, :replace, :none
     delta_window_length=9,
-    delta_matrix=:transposed,
-
+    delta_matrix=:standard, # :standard, :transposed
     # spectral
-    spectral_spectrum=:linear
+    spectral_spectrum=:linear # :linear, :linear_focused, :mel
 )
 
 # convert to Float64
@@ -65,8 +63,10 @@ data = signal_data(
 )
 
 takeFFT(data, setup)
-lin_spectrogram(data, setup)
 mel_spectrogram(data, setup)
-# _mfcc(data, setup)
-# spectral_features(data, setup)
-# f0(data, setup)
+_mfcc(data, setup)
+lin_spectrogram(data, setup)
+spectral_features(data, setup)
+f0(data, setup) # pay attention to fft length
+
+data.spectral_entropy
